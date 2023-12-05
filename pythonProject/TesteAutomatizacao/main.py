315757a42f989f5309c1a41552628049791e5f57
pg260 <spg2616@gmail.com>
2023-12-05 20:34:19 -0300

    coisinhas ai

--- pythonProject/TesteAutomatizacao/main.py
+++ pythonProject/TesteAutomatizacao/main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 import pyautogui
 import pyperclip
+import selenium
 import undetected_chromedriver as uc
 from pynput.mouse import Listener
 from selenium.webdriver import ActionChains
@@ -80,6 +81,14 @@ def CalculandoEntidadeBeneficente(beneficente):
         case "Sim":
             return 296
 
+def move_mouse_relative(x_rel, y_rel, duration=0.2):
+    largura_tela, altura_tela = pyautogui.size()
+    x = x_rel / 1920
+    y = y_rel / 1080
+    x_abs = int(x * largura_tela)
+    y_abs = int(y * altura_tela)
+    pyautogui.moveTo(x_abs, y_abs, duration=duration)
+
 cpfUser = input("Digite o cpf do usuário nesse formato '000.000.000-00': ").strip()
 
 print("Caso deseje pular essa etapa, apenas aperte enter.\n")
@@ -93,11 +102,12 @@ if(arm != ""):
     arm = input("Agora digite o DV: ").strip()
     conta.DV = arm
 
+
 driver = uc.Chrome()
 driver.get('https://conta.e-auditoria.com.br/Login')
 
 cpf_field = WebDriverWait(driver, 30).until(
-    EC.visibility_of_element_located((By.ID, 'Email'))
+    selenium.webdriver.support.expected_conditions.visibility_of_element_located((By.ID, 'Email'))
 )
 
 cpf_field.send_keys("janeiro19@hotmail.com")
@@ -108,12 +118,12 @@ senha_field.send_keys("Meucpf23")
 driver.find_element(By.ID, "lnkLogin").click()
 
 driver.maximize_window()
-pyautogui.moveTo(1729, 358, duration=1)
+move_mouse_relative(1729, 358)
 time.sleep(1)
 pyautogui.click()
 
 elemento = WebDriverWait(driver, 30).until(
-    EC.visibility_of_element_located((By.CLASS_NAME, "close"))
+    selenium.webdriver.support.expected_conditions.visibility_of_element_located((By.CLASS_NAME, "close"))
 )
 elemento.click()
 
@@ -122,7 +132,7 @@ button = buttons[4]
 button.click()
 
 elemento = WebDriverWait(driver, 30).until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, ".ant-btn-primary"))
+    selenium.webdriver.support.expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".ant-btn-primary"))
 )
 
 buttons = driver.find_elements(By.CSS_SELECTOR, ".ant-btn-primary")
@@ -130,7 +140,7 @@ button = buttons[1]
 button.click()
 
 elemento = WebDriverWait(driver, 30).until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, ".ant-btn-link"))
+    selenium.webdriver.support.expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".ant-btn-link"))
 )
 
 buttons = driver.find_elements(By.CSS_SELECTOR, ".ant-btn-link")
@@ -138,7 +148,7 @@ button = buttons[1]
 button.click()
 
 elemento = WebDriverWait(driver, 30).until(
-    EC.visibility_of_element_located((By.XPATH, f"//span[text()='{cpfUser}']"))
+    selenium.webdriver.support.expected_conditions.visibility_of_element_located((By.XPATH, f"//span[text()='{cpfUser}']"))
 )
 
 elemento_externo = driver.find_element(By.XPATH, f"//span[text()='{cpfUser}']/ancestor::div[contains(@style, 'width: 100%; display: flex;')]")
@@ -146,18 +156,18 @@ elemento_externo = elemento_externo.find_element(By.XPATH, ".//button[@title='Ed
 elemento_externo.click()
 
 modalEspera = WebDriverWait(driver, 30).until(
-    EC.visibility_of_element_located((By.ID, "modal-segurado"))
+    selenium.webdriver.support.expected_conditions.visibility_of_element_located((By.ID, "modal-segurado"))
 )
 
-pyautogui.moveTo(595, 652, duration=0.2)
+move_mouse_relative(595, 652)
 time.sleep(1)
 pyautogui.click()
 
-pyautogui.moveTo(595, 715, duration=0.2)
+move_mouse_relative(595, 715, duration=0.2)
 time.sleep(1)
 pyautogui.click()
 
-pyautogui.moveTo(789, 652, duration=0.2)
+move_mouse_relative(789, 652, duration=0.2)
 time.sleep(0.2)
 pyautogui.click()
 pyautogui.hotkey('ctrl', 'a')
@@ -168,7 +178,7 @@ else:
     pyautogui.hotkey('ctrl', 'c')
     codigo = pyperclip.paste()
 
-pyautogui.moveTo(960, 652, duration=0.2)
+move_mouse_relative(960, 652, duration=0.2)
 time.sleep(0.2)
 pyautogui.click()
 pyautogui.hotkey('ctrl', 'a')
@@ -179,7 +189,7 @@ else:
     pyautogui.hotkey('ctrl', 'c')
     agencia = pyperclip.paste()
 
-pyautogui.moveTo(1130, 652, duration=0.2)
+move_mouse_relative(1130, 652, duration=0.2)
 time.sleep(0.2)
 pyautogui.click()
 pyautogui.hotkey('ctrl', 'a')
@@ -190,7 +200,7 @@ else:
     pyautogui.hotkey('ctrl', 'c')
     numero = pyperclip.paste()
 
-pyautogui.moveTo(1300, 652, duration=0.2)
+move_mouse_relative(1300, 652, duration=0.2)
 time.sleep(0.2)
 pyautogui.click()
 pyautogui.hotkey('ctrl', 'a')
@@ -204,35 +214,35 @@ else:
 if(arm == ""):
     banco2 = Banco(codigo, agencia, numero, Dv)
 
-pyautogui.moveTo(1384, 716, duration=0.2)
+move_mouse_relative(1384, 716, duration=0.2)
 time.sleep(0.2)
 pyautogui.click()
 
-pyautogui.moveTo(37, 109, duration=0.2)
+move_mouse_relative(37, 109, duration=0.2)
 time.sleep(1)
 pyautogui.click()
 
 elemento = WebDriverWait(driver, 30).until(
-    EC.visibility_of_element_located((By.XPATH, f"//span[text()='{cpfUser}']"))
+    selenium.webdriver.support.expected_conditions.visibility_of_element_located((By.XPATH, f"//span[text()='{cpfUser}']"))
 )
 
 elemento.click()
 
 elemento = WebDriverWait(driver, 30).until(
-    EC.visibility_of_element_located((By.ID, "simple-tab-3"))
+    selenium.webdriver.support.expected_conditions.visibility_of_element_located((By.ID, "simple-tab-3"))
 )
 elemento.click()
 
-pyautogui.moveTo(1646, 915, duration=0.2)
+move_mouse_relative(1646, 915, duration=0.2)
 time.sleep(1)
 pyautogui.click()
 
-pyautogui.moveTo(1646, 990, duration=0.2)
+move_mouse_relative(1646, 990, duration=0.2)
 time.sleep(1)
 pyautogui.click()
 
 elemento = WebDriverWait(driver, 30).until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, 'button[title="Visualizar espelho do PER/DCOMP"]'))
+    selenium.webdriver.support.expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, 'button[title="Visualizar espelho do PER/DCOMP"]'))
 )
 
 totalRestituicoes = driver.find_elements(By.CSS_SELECTOR, 'button[title="Visualizar espelho do PER/DCOMP"]')
@@ -247,7 +257,7 @@ for i in range(len(totalRestituicoes)):
     total[contagem].click()
 
     elemento = WebDriverWait(driver, 30).until(
-        EC.visibility_of_element_located(
+        selenium.webdriver.support.expected_conditions.visibility_of_element_located(
             (By.CSS_SELECTOR, "td.MuiTableCell-root.MuiTableCell-body.MuiTableCell-alignLeft"))
     )
 
@@ -285,7 +295,7 @@ for i in range(len(totalRestituicoes)):
         elemento.click()
     
         elemento = WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located(
+            selenium.webdriver.support.expected_conditions.visibility_of_element_located(
                 (By.ID, "accountId"))
         )
         time.sleep(random.randint(1, 4))
@@ -323,98 +333,98 @@ for i in range(len(totalRestituicoes)):
     
     time.sleep(random.randint(4, 7))
 
-    pyautogui.moveTo(724, 368, duration=0.2)
+    move_mouse_relative(724, 368, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
 
-    pyautogui.moveTo(326, 458, duration=0.2)
+    move_mouse_relative(326, 458, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
 
-    pyautogui.moveTo(341,526, duration=0.2)
+    move_mouse_relative(341,526, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
 
-    pyautogui.moveTo(465, 565, duration=0.2)
+    move_mouse_relative(465, 565, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
 
-    pyautogui.moveTo(519, 599, duration=0.2)
+    move_mouse_relative(519, 599, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
 
-    pyautogui.moveTo(481, 639, duration=0.2)
-    time.sleep(0.2)
-    pyautogui.click()
-    
-    pyautogui.moveTo(325, 668, duration=0.2)
+    move_mouse_relative(481, 639, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
 
-    pyautogui.moveTo(1180, 532, duration=0.2)
+    move_mouse_relative(325, 668, duration=0.2)
+    time.sleep(0.2)
+    pyautogui.click()
+
+    move_mouse_relative(1180, 532, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
     DataAtual = datetime.today()
     pyautogui.typewrite(f"{pessoa.nome}//{DataAtual}", interval=0.02)
 
-    pyautogui.moveTo(1853, 994, duration=0.2)
+    move_mouse_relative(1853, 994, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
 
-    pyautogui.moveTo(583, 709, duration=0.2)
+    move_mouse_relative(583, 709, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
 
-    pyautogui.moveTo(992, 745, duration=0.2)
+    move_mouse_relative(992, 745, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
 
     time.sleep(random.randint(4,6))
 
-    pyautogui.moveTo(332, 673, duration=0.2)
+    move_mouse_relative(332, 673, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
     
     alturaOpcao = CalculandoPosicaoAno(restituicao.competenciaAno)
 
-    pyautogui.moveTo(332, alturaOpcao, duration=0.2)
+    move_mouse_relative(332, alturaOpcao, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
-    pyautogui.moveTo(622, 673, duration=0.2)
+    move_mouse_relative(622, 673, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
     
     alturaOpcao = CalculandoPosicaoMes(restituicao.competenciaMes)
 
-    pyautogui.moveTo(622, alturaOpcao, duration=0.2)
+    move_mouse_relative(622, alturaOpcao, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
-    pyautogui.moveTo(800, 673, duration=0.2)
+    move_mouse_relative(800, 673, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
     pyautogui.typewrite(f"{restituicao.nit}", interval=0.02)
 
-    pyautogui.moveTo(451, 868, duration=0.2)
+    move_mouse_relative(451, 868, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
     
     alturaOpcao = CalculandoCategoriaSegurado(pessoa.categoriaSegurado)
 
-    pyautogui.moveTo(451, alturaOpcao, duration=0.2)
+    move_mouse_relative(451, alturaOpcao, duration=0.2)
     time.sleep(1)
     pyautogui.click()
-    
-    pyautogui.moveTo(950, 868, duration=0.2)
+
+    move_mouse_relative(950, 868, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
 
-    pyautogui.moveTo(950, 908, duration=0.2)
+    move_mouse_relative(950, 908, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
-    pyautogui.moveTo(1860, 992, duration=0.2)
+    move_mouse_relative(1860, 992, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
@@ -422,67 +432,67 @@ for i in range(len(totalRestituicoes)):
     
     for cont in listaContribuicoes:
 
-        pyautogui.moveTo(1755, 500, duration=0.2)
+        move_mouse_relative(1755, 500, duration=0.2)
         time.sleep(1)
         pyautogui.click()
     
         time.sleep(random.randint(2, 4))
-    
-        pyautogui.moveTo(356, 670, duration=0.2)
+
+        move_mouse_relative(356, 670, duration=0.2)
         time.sleep(1)
         pyautogui.click()
-    
-        pyautogui.moveTo(356, 735, duration=0.2)
+
+        move_mouse_relative(356, 735, duration=0.2)
         time.sleep(0.2)
         pyautogui.click()
         pyautogui.typewrite(f"{cont.cnpj}", interval=0.02)
         
         posicao = CalculandoEntidadeBeneficente(cont.entidadeBeneficente)
-    
-        pyautogui.moveTo(posicao, 800, duration=0.2)
-        time.sleep(1)
-        pyautogui.click()
-    
-        pyautogui.moveTo(368, 850, duration=0.2)
-        time.sleep(0.2)
-        pyautogui.click()
-        pyautogui.typewrite(f"{cont.remuneracaoRecebida}", interval=0.02)
-    
-        pyautogui.moveTo(990, 850, duration=0.2)
-        time.sleep(0.2)
-        pyautogui.click()
-        pyautogui.typewrite(f"{cont.valorDescontado}", interval=0.02)
-    
-        pyautogui.moveTo(1526, 910, duration=0.2)
+
+        move_mouse_relative(posicao, 800, duration=0.2)
         time.sleep(1)
         pyautogui.click()
 
-    pyautogui.moveTo(1850, 993, duration=0.2)
+        move_mouse_relative(368, 850, duration=0.2)
+        time.sleep(0.2)
+        pyautogui.click()
+        pyautogui.typewrite(f"{cont.remuneracaoRecebida}", interval=0.02)
+
+        move_mouse_relative(990, 850, duration=0.2)
+        time.sleep(0.2)
+        pyautogui.click()
+        pyautogui.typewrite(f"{cont.valorDescontado}", interval=0.02)
+
+        move_mouse_relative(1526, 910, duration=0.2)
+        time.sleep(1)
+        pyautogui.click()
+
+    move_mouse_relative(1850, 993, duration=0.2)
     time.sleep(1)
     pyautogui.click()
     
     time.sleep(random.randint(2, 4))
 
-    pyautogui.moveTo(480, 523, duration=0.2)
+    move_mouse_relative(480, 523, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
     pyautogui.typewrite(f"{restituicao.valorPedRestituicao}", interval=0.02)
 
-    pyautogui.moveTo(1850, 993, duration=0.2)
+    move_mouse_relative(1850, 993, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
     time.sleep(random.randint(4, 6))
 
-    pyautogui.moveTo(330, 667, duration=0.2)
+    move_mouse_relative(330, 667, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
-    pyautogui.moveTo(330, 705, duration=0.2)
+    move_mouse_relative(330, 705, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
-    pyautogui.moveTo(665, 667, duration=0.2)
+    move_mouse_relative(665, 667, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
     if(arm != ""):
@@ -490,7 +500,7 @@ for i in range(len(totalRestituicoes)):
     else:
         pyautogui.typewrite(f"{banco2.Codigo}", interval=0.02)
 
-    pyautogui.moveTo(330, 740, duration=0.2)
+    move_mouse_relative(330, 740, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
     if(arm != ""):
@@ -498,7 +508,7 @@ for i in range(len(totalRestituicoes)):
     else:
         pyautogui.typewrite(f"{banco2.Agencia}", interval=0.02)
 
-    pyautogui.moveTo(740, 740, duration=0.2)
+    move_mouse_relative(740, 740, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
     if(arm != ""):
@@ -506,7 +516,7 @@ for i in range(len(totalRestituicoes)):
     else:
         pyautogui.typewrite(f"{banco2.NumeroConta}", interval=0.02)
 
-    pyautogui.moveTo(960, 740, duration=0.2)
+    move_mouse_relative(960, 740, duration=0.2)
     time.sleep(0.2)
     pyautogui.click()
     if(arm != ""):
@@ -514,26 +524,26 @@ for i in range(len(totalRestituicoes)):
     else:
         pyautogui.typewrite(f"{banco2.DV}", interval=0.02)
 
-    pyautogui.moveTo(1850, 995, duration=0.2)
+    move_mouse_relative(1850, 995, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
-    pyautogui.moveTo(1835, 995, duration=0.2)
+    move_mouse_relative(1835, 995, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
-    pyautogui.moveTo(100, 300, duration=0.2)
+    move_mouse_relative(100, 300, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
     driver.switch_to.window(driver.window_handles[0])
 
-    pyautogui.moveTo(33, 116, duration=0.2)
+    move_mouse_relative(33, 116, duration=0.2)
     time.sleep(1)
     pyautogui.click()
 
     elemento = WebDriverWait(driver, 30).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'button[title="Visualizar espelho do PER/DCOMP"]'))
+        selenium.webdriver.support.expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, 'button[title="Visualizar espelho do PER/DCOMP"]'))
     )
     
     contagem = contagem + 1
